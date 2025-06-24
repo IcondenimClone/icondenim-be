c2de3fe456d1ed10b3aded79491575e2dba010c9
Hải Tân <221124022310@due.udn.vn>
2025-06-24 17:27:33 +0700

    Hai commit v31

--- src/main/java/com/store/backend/order/controller/OrderController.java
+++ src/main/java/com/store/backend/order/controller/OrderController.java
@@ -6,16 +6,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store.backend.common.ApiResponse;
 import com.store.backend.order.entity.OrderEntity;
 import com.store.backend.order.mapper.OrderMapper;
+import com.store.backend.order.request.PlaceOrderRequest;
 import com.store.backend.order.response.OrderResponse;
 import com.store.backend.order.service.OrderService;
 import com.store.backend.user.customs.CustomUserDetails;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,10 +27,11 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
   private final OrderService orderService;
   private final OrderMapper orderMapper;
-  
+
   @PostMapping
-  public ResponseEntity<ApiResponse> placeOrder(@AuthenticationPrincipal CustomUserDetails userDetails) {
-    OrderEntity order = orderService.placeOrder(userDetails.getId());
+  public ResponseEntity<ApiResponse> placeOrder(@AuthenticationPrincipal CustomUserDetails userDetails,
+      @Valid @RequestBody PlaceOrderRequest request) {
+    OrderEntity order = orderService.placeOrder(userDetails.getId(), request);
     OrderResponse convertedOrder = orderMapper.entityToResponse(order);
     Map<String, Object> data = Map.of("order", convertedOrder);
     return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Tạo đơn hàng thành công", data));
--- src/main/java/com/store/backend/order/request/PlaceOrderRequest.java
+++ src/main/java/com/store/backend/order/request/PlaceOrderRequest.java
@@ -1,5 +1,12 @@
 package com.store.backend.order.request;
 
+import com.store.backend.order.enums.PaymentMethod;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +15,32 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlaceOrderRequest {
+  @NotBlank(message = "Yêu cầu tên người nhận")
+  @Size(max = 100, message = "Họ tên không quá 100 ký tự")
   private String fullName;
+
+  @NotBlank(message = "Yêu cầu số điện thoại đặt hàng")
+  @Size(max = 11, message = "Số điện thoại không quá 11 ký tự")
   private String phoneNumber;
+
+  @NotBlank(message = "Yêu cầu nhập địa chỉ cụ thể")
+  private String address;
+
+  @NotBlank(message = "Yêu cầu nhập xã/phường")
+  @Size(max = 50, message = "Xã/phường không vượt quá 50 ký tự")
+  private String commune;
+
+  @NotBlank(message = "Yêu cầu nhập quận/huyện")
+  @Size(max = 50, message = "Quận/huyện không vượt quá 50 ký tự")
+  private String district;
+
+  @NotBlank(message = "Yêu cầu nhập tỉnh/thành phố")
+  @Size(max = 50, message = "Tỉnh/thành phố không vượt quá 50 ký tự")
+  private String province;
+
+  @Enumerated(EnumType.STRING)
+  @NotNull(message = "Phương thức thanh toán là bắt buộc")
+  private PaymentMethod paymentMethod;
+  
+  private String note;
 }
