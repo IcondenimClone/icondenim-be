28a32f699153ce936cf5095d07f1296b0deda00d
Hải Tân <221124022310@due.udn.vn>
2025-06-25 14:08:01 +0700

    Hai commit v33

--- src/main/java/com/store/backend/smtp/EmailService.java
+++ src/main/java/com/store/backend/smtp/EmailService.java
@@ -1,5 +1,9 @@
 package com.store.backend.smtp;
 
+import com.store.backend.order.entity.OrderEntity;
+
 public interface EmailService {
   void sendAuthEmail(String to, String subject, String otp);
+
+  void sendOrderEmail(String to, String subject, OrderEntity order, String confirmLink);
 }
